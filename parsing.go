338df96806e8e57a9669cdc337ca561f2f57a26b
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-10-08 17:28:05 +0200

    improve format of package doc

--- parsing.go
+++ parsing.go
@@ -1,22 +1,22 @@
-/*Package parsing provides basic search and comparison of HTML documents.
+/*
+Package parsing provides basic search and comparison of HTML documents.
 To limit storage of references, it uses the net/html package and its Node type to structure HTML.
 
 Search a tag in a Node with options
- - searching a tag based on its name whatever attributes where its type is optional
- - searching a tag based on its non-pointer values: type, name, attribute and namespace
- - comparing tags including list of attributes where order is irrelevant
- - comparing Node structures with an optional type
+  - searching a tag based on its name whatever attributes where its type is optional
+  - searching a tag based on its non-pointer values: type, name, attribute and namespace
+  - comparing tags including list of attributes where order is irrelevant
+  - comparing Node structures with an optional type
 
 Three ways to print a node tree
- - select type of node and a the node value where to stop.
- - select type of nodes or none.
- - complete with indentation.
+  - select type of node and a the node value where to stop.
+  - select type of nodes or none.
+  - complete with indentation.
 
 Good to know
- - a non-matching closed tag is one element.
- - a non-closed tag is closed by the following opening tag.
-   The elements that follow are discarded as the tag is closed by the parser.
-
+  - a non-matching closed tag is one element.
+  - a non-closed tag is closed by the following opening tag.
+    The elements that follow are discarded as the tag is closed by the parser.
 */
 package parsing
 
